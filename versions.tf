8daf88602045f29c81125f0db9a346adecb38886
kaddoury990 <kamal.kaddoury@infogreffe-siege.fr>
2026-01-19 21:48:25 +0100

    Update Terraform required version and provider versions

--- versions.tf
+++ versions.tf
@@ -1,14 +1,13 @@
 terraform {
-  required_version = ">= 1.3.0"
-  
+  required_version = ">= 1.5.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.85.0" 
+      version = "~>4.16.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.5.1"
+      version = "~>3.0"
     }
   }
 }
